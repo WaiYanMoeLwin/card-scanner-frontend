51d466628bd9dfdcec33c1e52337ee67cc7bba30
WaiYanMoeLwin <waiyanmoelwin1710@gmail.com>
2026-09-01 09:51:05 +0700

    fix env variable

--- src/apis/inference.ts
+++ src/apis/inference.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const clientApi = axios.create({
-  baseURL: import.meta.env.BACKEND_SERVER_URL || 'http://localhost:3000/api/',
+  baseURL: import.meta.env.VITE_BACKEND_SERVER_URL || 'http://localhost:3000/api/',
   headers: {
     'Content-Type': 'application/json',
   },
